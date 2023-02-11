e764de752662d1320a03ab21e99406c2e66aa8ae
RosanneE <anderson.rosanne4@gmail.com>
2023-02-11 00:59:40 -0500

    about page content and image

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,36 @@
 function About(props) {
-    return (
-      <div className="about">
-    <h1>About</h1>
-    <p>"I am a Baltimore based Software Engineer.  Currently working as a Teaching Assistant at General Assembly's coding bootcamp, I enjoy helping students debug, reviewing difficult concepts with them, and teaching them how to google search even when your problem seems ungoogleable (have you tried to get information about 'this' lately?).\nHaving worked in various sectors before learning to coding, I am a huge beliver that working with people from diverse backgrounds, with differnnet lived experiences, creates novel solutions and change the way we look at problems.  I am passionate about learning new things, whether it's a new framework, coding language, instument or dance.  As poet E. E. Cummings wrote \"the goal of living is to grow\".  I can't wait to see where this journey takes me!"</p>
+  return (
+    <div className="about">
+      <h1>About</h1>
+      <div className="aboutDiv">
+      <div className="colOne">
+          <p className="pAbout">
+          &emsp;&emsp;I am a Baltimore based Software Engineer. Currently working as a
+            Teaching Assistant at General Assembly's coding bootcamp, I enjoy
+            helping students debug, reviewing difficult concepts with them, and
+            teaching them how to google search even when your problem seems
+            ungoogleable (have you tried to get information about 'this'
+            lately?).
+          </p>
+        </div>
+        <div className="colTwo">
+          <img className="aboutPic" src={require("../Assets/greenWall.jpeg")} />
+        </div>
+        <div className="colOne">
+          <p  className="pAbout">
+          &emsp;&emsp;Having worked in various sectors before learning to
+            coding, I am a huge beliver that working with people from diverse
+            backgrounds, with differnnet lived experiences, creates novel
+            solutions and change the way we look at problems. I am passionate
+            about learning new things, whether it's a new framework, coding
+            language, instument or dance. As poet E. E. Cummings wrote "the
+            goal of living is to grow". I can't wait to see where this journey
+            takes me!
+          </p>
+        </div>
+      </div>
     </div>
-    )
-  }
-  
-  export default About;
+  );
+}
+
+export default About;
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,17 +1,23 @@
+import React from 'react';
+import { Document, Page } from 'react-pdf';
+
 function Resume(props) {
   return (
     <div>
       <h1>Resume</h1>
-      <button
-        href="path/to/file.ext"
+      <a
+        href="RosanneAndersonResume.pdf"
         download
         rel="noopener noreferrer"
         target="_blank"
       >
         Download
-      </button>
+      </a>
+      <embed src="RosanneAndersonResume.pdf" width="500" height="375"></embed>
     </div>
   );
 }
 
 export default Resume;
+
+
